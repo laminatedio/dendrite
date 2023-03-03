df617849b50b70f578b22922ab656692a73df30a
Michael Chin <michaelchin@asta.hk>
2023-03-03 17:46:53 +0800

    modify cobra descriptions

--- cmd/root.go
+++ cmd/root.go
@@ -10,13 +10,7 @@ var cfgFile string
 
 var rootCmd = &cobra.Command{
 	Use:   "dendrite",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "service for retrieving configs",
 }
 
 func Execute() {
